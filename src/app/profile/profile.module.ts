b5e269ac527754a0c268759e20e356cfb4dc40d1
Hamza Sheikh <zafarhamza789@gmail.com>
2024-08-29 17:23:36 +0500

    complete

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -9,6 +10,7 @@ import { ProfilePageRoutingModule } from './profile-routing.module';
 import { ProfilePage } from './profile.page';
 
 @NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
     CommonModule,
     FormsModule,
--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {register} from 'swiper/element/bundle';
+register();
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
+  private readonly imagePaths: string[] = [
+    'assets/images/tom-cruise.jpg',
+    'assets/images/tom.jpg',
+    'assets/images/1672300616194.jpg',
+    'assets/images/player-holding-img.jpg',
+    'assets/images/1673873077921.jpg',
+    'assets/images/1673870935203.jpg',
+    'assets/images/football-img_1.jpg',
+    'assets/images/IMG_20221227_153113.jpg',
+    'assets/images/IMG_20220205_000705.jpg',
+    'assets/images/1673871421334.jpg',
+    'assets/images/player-holding-img.jpg',
+    'assets/images/IMG_20220205_000705.jpg'
+  ];
+
+  slides: { image: string }[] = [];
+
+
   constructor() { }
 
   ngOnInit() {
+    this.slides = this.imagePaths.map(path => ({ image: path }));
   }
 
 }
